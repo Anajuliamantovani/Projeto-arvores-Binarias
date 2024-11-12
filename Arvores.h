cfc4aca23e1ba377905423a8e91c4ae616dc803a
Ana Júlia Mantovani <anajuliamantovani001@gmail.com>
2024-11-12 19:25:31 -0300

    Criado o menu e arquivos .c e .h

---
+++ Arvores.h
@@ -0,0 +1,8 @@
+//
+// Created by julia-mantovani on 12/11/24.
+//
+
+#ifndef ARVORES_H
+#define ARVORES_H
+
+#endif //ARVORES_H
---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Arvores.h"
+
+int main(void)
+{
+    int option;
+
+    printf("\n\t==========================");
+    printf("\n\t|  O que deseja fazer ?  |");
+    printf("\n\t==========================\n");
+
+    printf("\t1. Adicionar\t2. Alterar\n\t3. Excluir\t\t4. Procurar\n\t5. Relatório\t6. Sair\n");
+    scanf("%d", &option);
+
+    switch (option)
+    {
+        case 1:
+
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        case 4:
+            break;
+
+        case 5:
+            break;
+
+        case 6:
+            return 0;
+    }
+
+    main();
+    return 0;
+}
